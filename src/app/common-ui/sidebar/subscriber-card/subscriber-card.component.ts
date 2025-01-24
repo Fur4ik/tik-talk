4fffc4f029acd93130e67ad7db9688e43ce44d96
Fur4ik <144418100+Fur4ik@users.noreply.github.com>
2025-01-24 15:51:35 +0500

    1.0

---
+++ src/app/common-ui/sidebar/subscriber-card/subscriber-card.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from '@angular/core';
+import {Profile} from '../../../data/interfaces/profile';
+import {ImgUrlPipe} from '../../../helpers/pipes/img-url.pipe';
+
+@Component({
+  selector: 'app-subscriber-card',
+  imports: [
+    ImgUrlPipe
+  ],
+  templateUrl: './subscriber-card.component.html',
+  standalone: true,
+  styleUrl: './subscriber-card.component.scss'
+})
+export class SubscriberCardComponent {
+  @Input() profile!: Profile
+
+
+}
---
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -0,0 +1,96 @@
+import {Component, effect, inject, signal, ViewChild} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ProfileService} from '../../data/services/profile.service';
+import {AuthService} from '../../auth/auth.service';
+import {firstValueFrom} from 'rxjs';
+import {AvatarUploadComponent} from './avatar-upload/avatar-upload.component';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-settings-page',
+  imports: [
+    ReactiveFormsModule,
+    AvatarUploadComponent
+  ],
+  templateUrl: './settings-page.component.html',
+  standalone: true,
+  styleUrl: './settings-page.component.scss'
+})
+export class SettingsPageComponent {
+  profileService = inject(ProfileService);
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  @ViewChild(AvatarUploadComponent) avatarUploader!: AvatarUploadComponent;
+
+  fb = new FormBuilder();
+
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    username: [{value: '', disabled: true}, Validators.required],
+    description: [''],
+    stack: [''],
+  })
+
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue({
+          ...this.profileService.me(),
+          stack: this.mergeStack(this.profileService.me()?.stack)
+        })
+      }
+    )
+  }
+
+
+  onSave() {
+    this.form.markAsTouched();
+    this.form.updateValueAndValidity();
+
+    if (this.form.invalid) return
+
+    if(this.avatarUploader.avatar)
+      firstValueFrom(this.profileService.uploadAvatar(this.avatarUploader.avatar))
+
+    firstValueFrom(
+    //@ts-ignore
+      this.profileService.patchProfile({
+        ...this.form.value,
+        stack: this.splitStack(this.form.value.stack)
+      })
+    )
+
+    this.router.navigate(['/profile/me']);
+
+  }
+
+  splitStack(stack: string | string[] | null | undefined): string[] {
+    if (!stack) return []
+    if(Array.isArray(stack)) return stack
+
+    return stack.split(',')
+  }
+
+  mergeStack(stack: string | string[] | null | undefined): string {
+    if(!stack) return ''
+    if(Array.isArray(stack)) return stack.join(',')
+
+    return stack
+  }
+
+
+  onCancel() {
+    // @ts-ignore
+    this.form.patchValue(this.profileService.me())
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+
+}
+
